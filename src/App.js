e42c3f2b5c67f7c7f1d4b895668e22fa12889b14
Jim Wolf <76077313+jim-wolf-i-am@users.noreply.github.com>
2025-06-19 15:09:58 -0500

    argh, moved assets/images into src because data.json wanted it that way

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-function App() {
+import "./styles.css";
+import data from "./data.json";
+console.log(data);
+export default function App() {
   return <div>Just a plain barebones React install.</div>;
 }
-
-export default App;
