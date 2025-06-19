7f1470baea1cb7519731b5ae6c78ba608a6dad9a
Jim Wolf <76077313+jim-wolf-i-am@users.noreply.github.com>
2025-06-19 16:17:54 -0500

    scaffold structure of components

--- src/App.js
+++ src/App.js
@@ -2,5 +2,39 @@ import "./styles.css";
 import data from "./data.json";
 console.log(data);
 export default function App() {
-  return <div>Just a plain barebones React install.</div>;
+  return (
+    <div className="component-wrapper">
+      <Result />
+      <Summary />
+    </div>
+  );
+}
+
+function Result() {
+  return <div>Your Results</div>;
+}
+
+function Summary() {
+  return (
+    <>
+      <div>Summary</div>
+      <ScoreItem />
+      <Button />
+    </>
+  );
+}
+
+function ScoreItem() {
+  return (
+    <div>
+      <div>icon</div>
+      <div>item name</div>
+      <div>score</div>
+      <div> / total_pts</div>
+    </div>
+  );
+}
+
+function Button() {
+  return <button>Continue</button>;
 }
